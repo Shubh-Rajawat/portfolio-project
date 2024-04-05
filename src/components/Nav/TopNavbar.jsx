4a23f6d0e4ca0b1e9bced02a846467883f30b8e7
Shubh-Rajawat <130832341+Shubh-Rajawat@users.noreply.github.com>
2024-04-05 10:38:24 +0530

    basic static page developed

--- src/components/Nav/TopNavbar.jsx
+++ src/components/Nav/TopNavbar.jsx
@@ -9,65 +9,65 @@ import LogoIcon from "../../assets/svg/Logo";
 import BurgerIcon from "../../assets/svg/BurgerIcon";
 
 export default function TopNavbar() {
-  const [y, setY] = useState(window.scrollY);
-  const [sidebarOpen, toggleSidebar] = useState(false);
+  const [ y, setY ] = useState( window.scrollY );
+  const [ sidebarOpen, toggleSidebar ] = useState( false );
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => setY(window.scrollY));
+  useEffect( () => {
+    window.addEventListener( "scroll", () => setY( window.scrollY ) );
     return () => {
-      window.removeEventListener("scroll", () => setY(window.scrollY));
+      window.removeEventListener( "scroll", () => setY( window.scrollY ) );
     };
-  }, [y]);
+  }, [ y ] );
 
 
   return (
     <>
-      <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
-      {sidebarOpen && <Backdrop toggleSidebar={toggleSidebar} />}
-      <Wrapper className="flexCenter animate whiteBg" style={y > 100 ? { height: "60px" } : { height: "80px" }}>
+      <Sidebar sidebarOpen={ sidebarOpen } toggleSidebar={ toggleSidebar } />
+      { sidebarOpen && <Backdrop toggleSidebar={ toggleSidebar } /> }
+      <Wrapper className="flexCenter animate whiteBg" style={ y > 100 ? { height: "60px" } : { height: "80px" } }>
         <NavInner className="container flexSpaceCenter">
-          <Link className="pointer flexNullCenter" to="home" smooth={true}>
+          <Link className="pointer flexNullCenter" to="home" smooth={ true }>
             <LogoIcon />
-            <h1 style={{ marginLeft: "15px" }} className="font20 extraBold">
+            <h1 style={ { marginLeft: "15px" } } className="font20 extraBold">
               fanatic
             </h1>
           </Link>
-          <BurderWrapper className="pointer" onClick={() => toggleSidebar(!sidebarOpen)}>
+          <BurderWrapper className="pointer" onClick={ () => toggleSidebar( !sidebarOpen ) }>
             <BurgerIcon />
           </BurderWrapper>
           <UlWrapper className="flexNullCenter">
             <li className="semiBold font15 pointer">
-              <Link activeClass="active" style={{ padding: "10px 15px" }} to="home" spy={true} smooth={true} offset={-80}>
+              <Link activeClass="active" style={ { padding: "10px 15px" } } to="home" spy={ true } smooth={ true } offset={ -80 }>
                 Home
               </Link>
             </li>
             <li className="semiBold font15 pointer">
-              <Link activeClass="active" style={{ padding: "10px 15px" }} to="services" spy={true} smooth={true} offset={-80}>
+              <Link activeClass="active" style={ { padding: "10px 15px" } } to="services" spy={ true } smooth={ true } offset={ -80 }>
                 Services
               </Link>
             </li>
             <li className="semiBold font15 pointer">
-              <Link activeClass="active" style={{ padding: "10px 15px" }} to="projects" spy={true} smooth={true} offset={-80}>
+              <Link activeClass="active" style={ { padding: "10px 15px" } } to="projects" spy={ true } smooth={ true } offset={ -80 }>
                 Projects
               </Link>
             </li>
             <li className="semiBold font15 pointer">
-              <Link activeClass="active" style={{ padding: "10px 15px" }} to="blog" spy={true} smooth={true} offset={-80}>
+              <Link activeClass="active" style={ { padding: "10px 15px" } } to="blog" spy={ true } smooth={ true } offset={ -80 }>
                 Blog
               </Link>
             </li>
             <li className="semiBold font15 pointer">
-              <Link activeClass="active" style={{ padding: "10px 15px" }} to="pricing" spy={true} smooth={true} offset={-80}>
+              <Link activeClass="active" style={ { padding: "10px 15px" } } to="pricing" spy={ true } smooth={ true } offset={ -80 }>
                 Pricing
               </Link>
             </li>
             <li className="semiBold font15 pointer">
-              <Link activeClass="active" style={{ padding: "10px 15px" }} to="contact" spy={true} smooth={true} offset={-80}>
+              <Link activeClass="active" style={ { padding: "10px 15px" } } to="contact" spy={ true } smooth={ true } offset={ -80 }>
                 Contact
               </Link>
             </li>
           </UlWrapper>
-          <UlWrapperRight className="flexNullCenter">
+          {/* <UlWrapperRight className="flexNullCenter">
             <li className="semiBold font15 pointer">
               <a href="/" style={{ padding: "10px 30px 10px 0" }}>
                 Log in
@@ -78,7 +78,7 @@ export default function TopNavbar() {
                 Get Started
               </a>
             </li>
-          </UlWrapperRight>
+          </UlWrapperRight> */}
         </NavInner>
       </Wrapper>
     </>
